7c3f9426f228217f9b076e21910ad1c809853472
TonyCha3713 <tonyjcha3713@gmail.com>
2021-09-24 14:17:29 -0700

    test

--- Sept-Present/src/main.cpp
+++ Sept-Present/src/main.cpp
@@ -74,6 +74,7 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
+	std::cout << "testing" << std::endl;
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	pros::Motor left_mtr(1);
 	pros::Motor right_mtr(2);
